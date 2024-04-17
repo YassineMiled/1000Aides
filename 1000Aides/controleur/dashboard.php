7409665e99eb90c35c75c6ba5713abfc7774b264
YassineMiled <146331534+YassineMiled@users.noreply.github.com>
2024-04-18 00:14:37 +0200

    Delete 1000Aides directory

--- 1000Aides/controleur/dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
-    $racine="..";
-}
-include "$racine/vue/VueDashboard.php";
-
-?>
--- 1000Aides/controleur/signUp.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
-    $racine="..";
-}
-
-include_once "$racine/modele/SignUp.inc.php";
-include "$racine/vue/VueSignUp.php";
-?>
